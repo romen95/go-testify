dfcc596a81a93abb07e63bc68e0fac93a2230ada
Roman Retinskiy <romanretinskiy@yandex.ru>
2023-12-27 13:35:43 +0300

    Refactor code

--- precode_test.go
+++ precode_test.go
@@ -1,6 +1,7 @@
 package precode
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -10,46 +11,41 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/cafe?count=5&city=moscow", nil)
-
-	expectedCount := len(cafeList["moscow"])
+func GetResponse(url string) *httptest.ResponseRecorder {
+	request := httptest.NewRequest(http.MethodGet, url, nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	handler.ServeHTTP(responseRecorder, request)
 
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	return responseRecorder
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	expectedCount := len(cafeList["moscow"])
+
+	responseRecorder := GetResponse(fmt.Sprintf("/cafe?count=%d&city=moscow", expectedCount+5))
+
 	require.NotEmpty(t, responseRecorder.Body)
 	body := strings.Split(responseRecorder.Body.String(), ",")
 	assert.Len(t, body, expectedCount)
 }
 
 func TestMainHandlerWhenCorrectRequest(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/cafe?count=5&city=moscow", nil)
-
-	expectedCode := http.StatusOK
 	expectedCount := len(cafeList["moscow"])
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := GetResponse(fmt.Sprintf("/cafe?count=%d&city=moscow", expectedCount+5))
 
-	assert.Equal(t, expectedCode, responseRecorder.Code)
 	require.NotEmpty(t, responseRecorder.Body)
 	body := strings.Split(responseRecorder.Body.String(), ",")
 	assert.Len(t, body, expectedCount)
 }
 
 func TestMainHandlerWhenIncorrectCity(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/cafe?count=5&city=vladyvostok", nil)
-
 	expectedCode := http.StatusBadRequest
 	expectedBody := `wrong city value`
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := GetResponse("/cafe?count=5&city=vladyvostok")
 
 	assert.Equal(t, expectedCode, responseRecorder.Code)
 	require.NotEmpty(t, responseRecorder.Body)
